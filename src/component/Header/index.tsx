4c3161345a9e3ca19da3e798ba6e4be03c2caa1a
minjyo <minjyo8823@gmail.com>
2019-11-06 11:14:50 +0900

    mage page structure

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header: React.SFC<IProps> = props => {
-    return <div>header!!!</div>;
+    return <div className="header">header!!!</div>;
 };
 
 type IProps = {};
--- src/screens/Main/MainPT.tsx
+++ src/screens/Main/MainPT.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Header from "component/Header";
+import "./Main.scss";
 
 const MainPT: React.SFC<IProps> = props => {
     return (
         <div className="wrap">
             <Header></Header>
+            <div className="page">
+                <div className="intro">intro</div>
+                <div className="club">
+                    <div className="title">club</div>
+                    <div className="itembox">
+                        <div className="item">item1</div>
+                        <div className="item">item2</div>
+                        <div className="item">item3</div>
+                    </div>
+                </div>
+                <div className="education">education</div>
+                <div className="conference">conference</div>
+            </div>
+            <div className="footer">footer</div>
         </div>
     );
 };
